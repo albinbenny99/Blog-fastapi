cf625cc9418c20e82ff2f2c0c174b409072c01cf
ALBIN BENNY <albin99.in@gmail.com>
2024-01-07 16:07:28 +0530

    Before database connection

--- main.py
+++ main.py
@@ -1,9 +1,36 @@
 from fastapi import FastAPI
-app=FastAPI()
-@app.get('/')
-def index():
-  return {'data':{'name':'albi'}}
+from typing import Optional
+from pydantic import BaseModel
 
-@app.get('/about')
-def about():
-  return {'data':{'about'}}
+app = FastAPI()
+
+@app.get('/blog')
+def index(limit=10,published:bool=True,sort:Optional[str] = None):
+    if published:
+        return {'data': f'{limit} published blog list'}
+    else:
+        return {'data': f'{limit}blogs from the db'}
+
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data': 'all unpublished blogs'}
+
+@app.get('/blog/{id}')
+def show(id: int):
+    return {'data': id}
+
+
+
+@app.get('/blog/{id}/comments')
+def comments(id,limit=10):
+    return limit
+    return {'data': {'1', '2'}}
+
+class Blog(BaseModel):
+    title:str
+    body:str
+    published:Optional[bool]=None
+
+@app.post('/blog')
+def create_blog(blog:Blog):
+    return{'data':f'blog is created with title as {blog.title}'}
